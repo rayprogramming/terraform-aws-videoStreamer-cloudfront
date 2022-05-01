572ede43cc65f9c29f4f636dd60b34edbfe08b6a
James Ray <rihoj1@gmail.com>
2022-05-01 02:32:37 -0400

    fix(s3): fixed the values I expected

--- ssm.tf
+++ ssm.tf
@@ -1,5 +1,11 @@
 resource "aws_ssm_parameter" "cdn_bucket" {
   name        = "/${var.name}/cdn/bucket"
+  description = "The bucket name for the cdn"
+  type        = "String"
+  value       = module.s3_one.s3_bucket_id
+}
+resource "aws_ssm_parameter" "cdn_bucket_domain" {
+  name        = "/${var.name}/cdn/domain"
   description = "The bucket domain name for the cdn"
   type        = "String"
   value       = module.s3_one.s3_bucket_bucket_domain_name
