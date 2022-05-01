7f0bae4eba1a8be95a372d6eaaa84894fb342c30
James Ray <rihoj1@gmail.com>
2022-05-01 01:31:44 -0400

    fix(s3): remove static value of s3 policy

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${module.s3_one.s3_bucket_arn}/static/*"]
+    resources = ["${module.s3_one.s3_bucket_arn}/*"]
 
     principals {
       type        = "AWS"
